c5d8cf3b16d80495d608ccbfd0ba0c6a5705fd89
Quentin Rouet <quentinrouet@hotmail.fr>
2022-02-15 16:44:15 +0100

    Ajout Serializable

--- androkado/src/main/java/com/quentinrouet/androkado/Article.java
+++ androkado/src/main/java/com/quentinrouet/androkado/Article.java
@@ -1,9 +1,12 @@
 package com.quentinrouet.androkado;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by quentin for Constraint Layout on 15/02/2022.
  */
-public class Article {
+public class Article implements Parcelable {
     private String nom, description, url;
     private float prix;
     private double degreEnvie;
@@ -17,6 +20,42 @@ public class Article {
         this.degreEnvie = degreEnvie;
         this.isAchete = isAchete;
     }
+
+    protected Article(Parcel in) {
+        nom = in.readString();
+        description = in.readString();
+        url = in.readString();
+        prix = in.readFloat();
+        degreEnvie = in.readDouble();
+        isAchete = in.readByte() != 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(nom);
+        dest.writeString(description);
+        dest.writeString(url);
+        dest.writeFloat(prix);
+        dest.writeDouble(degreEnvie);
+        dest.writeByte((byte) (isAchete ? 1 : 0));
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Article> CREATOR = new Creator<Article>() {
+        @Override
+        public Article createFromParcel(Parcel in) {
+            return new Article(in);
+        }
+
+        @Override
+        public Article[] newArray(int size) {
+            return new Article[size];
+        }
+    };
 
     public String getNom() {
         return nom;
